263c67c0422daa8587ed42d655ce6d4574616b0b
DimitarMalamski <mitemalamata@gmail.com>
2025-10-02 00:10:55 +0200

    Test

--- backend/src/main/java/com/careerpath/controller/UserController.java
+++ backend/src/main/java/com/careerpath/controller/UserController.java
@@ -1,5 +1,5 @@
 package com.careerpath.controller;
-// hello world! 2.0
+// hello world! 3.0
 import com.careerpath.dto.UserDto;
 import com.careerpath.mapper.UserMapper;
 import com.careerpath.model.User;
