e0d95208eb6ac4601bf40c295767ec59fd39c861
DimitarMalamski <mitemalamata@gmail.com>
2025-10-06 15:12:24 +0200

    Extracting the Login button into a separate component

---
+++ frontend/src/app/components/login-button/login-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login-button',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './login-button.component.html'
+})
+
+export class LoginButtonComponent { }
--- frontend/src/app/components/shared/navbar/navbar.component.ts
+++ frontend/src/app/components/shared/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LoginButtonComponent } from '../../login-button/login-button.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LoginButtonComponent],
   templateUrl: './navbar.component.html'
 })
 
