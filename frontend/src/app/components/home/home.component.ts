f1bf73da4f792eba9c02d455e1c53f3d75867246
DimitarMalamski <mitemalamata@gmail.com>
2025-10-14 17:49:19 +0200

    Creating initial structure for how-it-works section

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { HowItWorksSectionComponent } from './how-it-works-section/how-it-works-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeroSectionComponent],
+  imports: [CommonModule, HeroSectionComponent, HowItWorksSectionComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {}
---
+++ frontend/src/app/components/home/how-it-works-section/how-it-works-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-how-it-works-section',
+  standalone: true,
+  templateUrl: './how-it-works-section.component.html',
+})
+
+export class HowItWorksSectionComponent {}
