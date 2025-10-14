29913930dc2e963a18158adb22a7ac4d2a4fc99c
DimitarMalamski <mitemalamata@gmail.com>
2025-10-15 00:42:22 +0200

    Making navbar mobile responsive + fixing colour scheme

--- frontend/src/app/components/shared/navbar/navbar.component.ts
+++ frontend/src/app/components/shared/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { LoginButtonComponent } from '../login-button/login-button.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule, LoginButtonComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html'
 })
 
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
