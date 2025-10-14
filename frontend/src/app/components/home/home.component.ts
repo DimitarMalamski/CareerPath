00b410328633a60899496a8ea6ef6e10ccdcc891
DimitarMalamski <mitemalamata@gmail.com>
2025-10-14 16:32:49 +0200

    Saving current state of the project before branching

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { SectionColorDirective } from '../../shared/section-color.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeroSectionComponent],
+  imports: [CommonModule, HeroSectionComponent, SectionColorDirective],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {}
