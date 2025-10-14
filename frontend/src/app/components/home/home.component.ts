5637b7ff7a70e31f4b79091189936e84d7bdef79
DimitarMalamski <mitemalamata@gmail.com>
2025-10-14 17:17:07 +0200

    Sratching the colour switching idea

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
-import { SectionColorDirective } from '../../shared/section-color.directive';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeroSectionComponent, SectionColorDirective],
+  imports: [CommonModule, HeroSectionComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {}
