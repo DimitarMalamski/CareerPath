011a05f528a0d6ec44e22bc5dd5c7d5fa12dc49b
DimitarMalamski <mitemalamata@gmail.com>
2025-10-15 14:13:14 +0200

    Make animations to the hero component in the home page + button bounsing

--- frontend/src/app/components/home/hero-section/hero-section.component.ts
+++ frontend/src/app/components/home/hero-section/hero-section.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero-section.component.html',
 })
 
-export class HeroSectionComponent {}
+export class HeroSectionComponent implements OnInit {
+  ngOnInit() {
+    AOS.init({
+      duration: 800,
+      once: true
+    });
+  }
+}
