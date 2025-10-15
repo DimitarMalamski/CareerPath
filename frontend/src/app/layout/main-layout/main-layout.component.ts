4c397837d56f95ec353706d31bbfcdeed874988e
DimitarMalamski <mitemalamata@gmail.com>
2025-10-15 17:25:55 +0200

    Creating initial footer design

--- frontend/src/app/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/layout/main-layout/main-layout.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../components/shared/navbar/navbar.component';
+import { FooterComponent } from '../../components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterModule, NavbarComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './main-layout.component.html',
 })
 
