045c2da9a211dec23c788bf01aa58fedda1be009
DimitarMalamski <mitemalamata@gmail.com>
2025-10-17 17:03:29 +0200

    Add test for app.ts

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { App } from './app';
 
+const AOS = require('aos');
+AOS.init = jasmine.createSpy('init');
+
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,5 +24,17 @@ describe('App', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
+
+  it('should call AOS.init on ngOnInit', () => {
+    const fixture = TestBed.createComponent(App);
+    const app = fixture.componentInstance;
+
+    app.ngOnInit();
+
+    expect(AOS.init).toHaveBeenCalledWith({
+      duration: 700,
+      once: true,
+    });
   });
 });
