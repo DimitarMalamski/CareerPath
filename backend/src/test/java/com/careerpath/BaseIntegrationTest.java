1c11b7964f314d1be2a4c16451a52dab855d07c7
DimitarMalamski <mitemalamata@gmail.com>
2025-11-09 01:00:56 +0100

    Separating the CI/CD into different jobs

--- backend/src/test/java/com/careerpath/BaseIntegrationTest.java
+++
@@ -1,9 +0,0 @@
-package com.careerpath;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@SpringBootTest
-@ActiveProfiles("test")
-public abstract class BaseIntegrationTest {
-}
