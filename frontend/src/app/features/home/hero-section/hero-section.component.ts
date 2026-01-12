0b0dba1561e0aba1ee7295d40f1a3aaaf0f74511
DimitarMalamski <mitemalamata@gmail.com>
2026-01-12 18:46:54 +0100

    Fixing button routing

--- frontend/src/app/features/home/hero-section/hero-section.component.ts
+++ frontend/src/app/features/home/hero-section/hero-section.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import * as AOS from 'aos';
+  import { Component, OnInit } from '@angular/core';
+  import { CommonModule } from '@angular/common';
+  import { RouterModule } from '@angular/router';
+  import * as AOS from 'aos';
 
-@Component({
-  selector: 'app-hero-section',
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule,
-  ],
-  templateUrl: './hero-section.component.html',
-})
+  @Component({
+    selector: 'app-hero-section',
+    standalone: true,
+    imports: [
+      CommonModule,
+      RouterModule,
+    ],
+    templateUrl: './hero-section.component.html',
+  })
 
-export class HeroSectionComponent implements OnInit {
-  ngOnInit() {
-    AOS.init({
-      duration: 800,
-      once: true
-    });
+  export class HeroSectionComponent implements OnInit {
+    ngOnInit() {
+      AOS.init({
+        duration: 800,
+        once: true
+      });
+    }
   }
-}
