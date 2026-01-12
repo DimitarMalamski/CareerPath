dee5e060545711b8ad456f2bf09d42386b3faa8c
DimitarMalamski <mitemalamata@gmail.com>
2026-01-12 21:40:14 +0100

    Change a URl

--- frontend/src/app/core/services/job-websocket.service.ts
+++ frontend/src/app/core/services/job-websocket.service.ts
@@ -27,7 +27,7 @@ export class JobWebsocketService {
     const SockJS = (await import('sockjs-client')).default;
 
     this.client = new Client({
-      webSocketFactory: () => new SockJS('http://localhost:8080/ws'),
+      webSocketFactory: () => new SockJS('https://careerpath-ip.com/ws'),
       reconnectDelay: 5000,
       debug: (msg) => console.log('[STOMP]', msg)
     });
